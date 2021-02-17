21bf9af6ae309756b0f36be5c64126443c869356
SashaPlavi <sasha.bingulac@gmail.com>
2021-02-17 17:49:04 +0100

    action, reducer, conect, delete

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 
 class _App extends Component<AppProps> {
-  componentDidMount() {
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
+  onBtnClick = (): void => {
     this.props.fetchTodos();
+  };
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
+    });
   }
   render() {
-    return <div>hi mf!</div>;
+    return (
+      <div>
+        <button onClick={this.onBtnClick}>Fetch Todos</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
@@ -22,4 +41,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,10 +1,12 @@
-import { Todo, FetchTodosAction } from '../actions/index';
-import { ActionTypes } from '../actions/types';
+import { Todo, Action, ActionTypes } from '../actions/index';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
